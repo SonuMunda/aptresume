6053d612c20150513074c25e23643c0f162822d2
SonuMunda <sonumunda1312.gmail.com>
2025-07-21 20:01:30 +0530

    removed image upload component

--- ui/resume_builder/styles/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Roboto, Lato, Nunito, Montserrat, Open_Sans } from "next/font/google";
-
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
-const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
-const nunito = Nunito({ subsets: ["latin"], weight: ["400", "700"] });
-const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
-const openSans = Open_Sans({ subsets: ["latin"], weight: ["400", "700"] });
-
-export { roboto, lato, nunito, montserrat, openSans };
